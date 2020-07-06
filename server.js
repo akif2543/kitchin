b7c63fa1a8ee583b39a8a7ee11be8104c4c8e6c8
Akif Saifi <akifsaifi@gmail.com>
2020-07-06 11:43:29 -0400

    redo file directory, swapping server and frontend folders

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongoose.set("useFindAndModify", false);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
