9acef13d0859de7a2eab6650508bfff0e130b6bd
Akif Saifi <akifsaifi@gmail.com>
2020-07-06 11:56:25 -0400

    Just comment out logger and rdt for now

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,30 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+// import logger from "redux-logger";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "../reducers/root_reducer";
 
-let configureStore;
+// let configureStore;
 
-if (process.env.NODE_ENV !== "production") {
-  import logger from "redux-logger";
-  import { composeWithDevTools } from "redux-devtools-extension";
-
-  configureStore = (preloadedState) =>
-    createStore(
-      rootReducer,
-      preloadedState,
-      composeWithDevTools(applyMiddleware(thunk, logger))
-    );
-} else {
-  configureStore = (preloadedState) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk));
-}
-
+// if (process.env.NODE_ENV !== "production") {
+//   configureStore = (preloadedState) =>
+//     createStore(
+//       rootReducer,
+//       preloadedState,
+//       composeWithDevTools(applyMiddleware(thunk, logger))
+//     );
+// } else {
 const configureStore = (preloadedState) =>
-  createStore(
-    rootReducer,
-    preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
-  );
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+// }
 
 export default configureStore;
