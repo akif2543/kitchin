dbf0cf34273e5c59e3c572bfc42871daf488e683
Akif Saifi <akifsaifi@gmail.com>
2020-07-06 12:04:52 -0400

    once more

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose
 mongoose.set("useFindAndModify", false);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
