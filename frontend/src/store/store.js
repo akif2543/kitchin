9dff185bec511f2bd04ff2e49588d9404179efaf
Akif Saifi <akifsaifi@gmail.com>
2020-07-06 11:52:31 -0400

    Add conditional to ignore redux dev tools and logger in production

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "../reducers/root_reducer";
+
+let configureStore;
+
+if (process.env.NODE_ENV !== "production") {
+  import logger from "redux-logger";
+  import { composeWithDevTools } from "redux-devtools-extension";
+
+  configureStore = (preloadedState) =>
+    createStore(
+      rootReducer,
+      preloadedState,
+      composeWithDevTools(applyMiddleware(thunk, logger))
+    );
+} else {
+  configureStore = (preloadedState) =>
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+}
 
 const configureStore = (preloadedState) =>
   createStore(
